5c1ecc70d98b9ad5075cc87812fec5d8cffd37d9
fernando8534 <fernandotakara143@gmail.com>
2024-12-07 14:12:46 -0300

    Documentação e remoção de linha desnecessária

--- ep3.py
+++ ep3.py
@@ -6,6 +6,7 @@ class Peca:
     def __init__(self, tipo, posicao):
         self.tipo = tipo
         self.formas = {
+            #Aqui é um dicionario com de todas as peças com as 4 possíveis rotações delas, sendo a da posição 0 a padrão.
             "I": [["....", "####", "....", "...."], [".#..", ".#..", ".#..", ".#.."], ["....", "....", "####", "...."], ["..#.", "..#.", "..#.", "..#."]],
             "O": [["##", "##"], ["##", "##"], ["##", "##"], ["##", "##"]],
             "T": [[".#.", "###", "..."], [".#.", ".##", ".#."], ["...", "###", ".#."], [".#.", "##.", ".#."]],
@@ -19,6 +20,7 @@ class Peca:
         self.x, self.y = 0, posicao
 
     def rotacionar(self, sentido):
+        #Fazemos a conta da rotação atual com sua respectiva rotação mod4
         if(sentido == "R"):
             self.rotacao = (self.rotacao + 1) % 4
         elif(sentido == "L"):
@@ -29,10 +31,12 @@ class Partida:
     def __init__(self, linhas, colunas):
         self.linhas = linhas
         self.colunas = colunas
+        #Gera o tabuleiro vazio
         self.tabuleiro = [["." for _ in range(colunas)] for _ in range(linhas)]
         self.peca_atual = self._gerar_peca()
 
     def _gerar_peca(self):
+        #Gera a peça aleatória
         tipos = ["I", "O", "T", "S", "Z", "J", "L"]
         return Peca(random.choice(tipos), self.colunas//2 - 2)
     
@@ -47,6 +51,7 @@ class Partida:
         for i, row in enumerate(self.peca_atual.matriz):
             for j, bloco in enumerate(row):
                 if bloco == "#":
+                    #Checa se é permitido a movimentação do usuário
                     nx, ny = self.peca_atual.x + i + dx, self.peca_atual.y + j + dy
                     if nx >= self.linhas or ny < 0 or ny >= self.colunas or self.tabuleiro[nx][ny] == "#":
                         return False
@@ -56,20 +61,23 @@ class Partida:
         antiga_matriz = self.peca_atual.matriz
         antiga_rotacao = self.peca_atual.rotacao
         self.peca_atual.rotacionar(posicao)
-
+        #Checa se a rotação é válida
         if not self._pode_mover(0, 0):
             self.peca_atual.matriz = antiga_matriz
             self.peca_atual.rotacao = antiga_rotacao
 
 
     def _fixar_peca(self):
+        #Coloca a peça no tabuleiro principal
         for i, row in enumerate(self.peca_atual.matriz):
             for j, bloco in enumerate(row):
                 if bloco == "#":
                     self.tabuleiro[self.peca_atual.x + i][self.peca_atual.y + j] = "#"
+        #Gera a nova peça após de fixar a antiga
         self.peca_atual = self._gerar_peca()
 
     def imprimir_tabuleiro(self):
+        #Imprime o estado do tabuleiro com a peça que o usuário está movimentando
         tabuleiro_temporario = [row[:] for row in self.tabuleiro]
 
         for i, row in enumerate(self.peca_atual.matriz):
@@ -111,7 +119,6 @@ class Jogo:
 def iniciar_partida():
     linhas = int(input("Digite o número de linhas da tela do jogo: "))
     colunas = int(input("Digite o número de colunas da tela do jogo: "))
-    partida = Partida(linhas, colunas)
     jogo = Jogo(linhas, colunas)
     jogo.iniciar()
     
