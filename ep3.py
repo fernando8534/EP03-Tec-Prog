eae5b35273cf914086d41e0c6d64a4863c8fc841
natan <natanmd604@gmail.com>
2024-12-15 22:18:44 -0300

    Ajeita o bug quando gera a documentação html

--- ep3.py
+++ ep3.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
-"""!
-@file ep3.py
-@brief Um arquivo que implementa uma versão do jogo Tetris chamada Textris
-"""
 import os, random, readchar, pickle, datetime
 
-"""!
-@class Peca
 
-@brief Implementa as funcionalidades específicas de cada peça de Textris
-
-@details A classe implementa as peças de jogo com base em suas características de tipo, forma e posição no tabuleiro de Tetris.
-"""
 class Peca:
-    """!
-    @brief Construtor da peça
-    
-    @param tipo Tipo (formato) da peça
-    @param posicao Posição da peça no tabuleiro
     """
+    Classe: Peca
+
+    Resumo: Implementa as funcionalidades específicas de cada peça de Textris
+
+    Detalhes: A classe implementa as peças de jogo com base em suas características de tipo, forma e posição no tabuleiro de Tetris.
+    """
+    
     def __init__(self, tipo, posicao):
+        """
+        Resumo: Construtor da peça
+        
+        Parâmetro tipo: Tipo (formato) da peça
+        Parâmetro posicao: Posição da peça no tabuleiro
+        """
         self.tipo = tipo
         self.formas = {
             #Aqui é um dicionário de todas as peças com as 4 possíveis rotações delas, sendo a da posição 0 a padrão.
@@ -35,17 +33,17 @@ class Peca:
         self.matriz = self.formas[self.tipo][self.rotacao]
         self.x, self.y = 0, posicao
 
-    """!
-    @brief Rotaciona a peça
-    
-    @details Baseia-se nas listas do dicionário "formas" - que contém todas as possíveis rotações de cada peça - para decidir qual
-    forma de rotação a peça assumirá. Caso a rotação seja para a direita, acessa a próxima posição da lista em questão, caso seja 
-    para a esquerda, acessa a posição anterior. Todas as contas são feitas utilizando aritmética modular com o intuito de manter a
-    propriedade cíclica da rotação.
-
-    @param sentido Sentido de rotação da peça. R é horário, enquanto L se trata do sentido anti-horário.
-    """
     def rotacionar(self, sentido):
+        """
+        Resumo: Rotaciona a peça
+
+        Detalhes: Baseia-se nas listas do dicionário "formas" - que contém todas as possíveis rotações de cada peça - para decidir qual
+        forma de rotação a peça assumirá. Caso a rotação seja para a direita, acessa a próxima posição da lista em questão, caso seja 
+        para a esquerda, acessa a posição anterior. Todas as contas são feitas utilizando aritmética modular com o intuito de manter a
+        propriedade cíclica da rotaçã   o.
+
+        Parâmetro sentido: Sentido de rotação da peça. R é horário, enquanto L se trata do sentido anti-horário.
+        """
         #Fazemos a conta da rotação atual com sua respectiva rotação mod4
         if(sentido == "R"):
             self.rotacao = (self.rotacao + 1) % 4
@@ -53,37 +51,36 @@ class Peca:
             self.rotacao = (self.rotacao + 3) % 4
         self.matriz = self.formas[self.tipo][self.rotacao]
     
-    """!
-    @brief Retorna o tipo da peça
-    """
     def retorna_tipo(self):
+        """
+        Resumo: Retorna o tipo da peça
+        """
         return self.tipo
 
-"""!
-@class Partida
-
-@brief Implementa todos os aspectos inerentes a uma instância de partida de Textris
-
-@details Atribui a um objeto as características de linhas, colunas, tabuleiro - que se trata da combinação de linhas e colunas, além
-das peças já fixadas, peças que ainda não foram fixadas - mas já apareceram no tabuleiro (parâmetro importante especialmente para a 
-funcionalidade de gravar o jogo), do jogador e sua respectiva pontuação.
-"""
 class Partida:
-    """!
-    @brief Construtor para uma instância de Partida
-
-    @details As linhas e colunas formam o tabuleiro no qual os blocos se empilharão, cada partida também tem necessariamente um jogador 
-    atribuído a ela. Os argumentos opcionais "tabuleiro", "peca_atual" e "pontuacao" são passados para carregar uma partida 
-    previamente gravada.
-
-    @param linhas Quantidade de linhas do tabuleiro 
-    @param colunas Quantidade de colunas do tabuleiro 
-    @param jogador Jogador da partida
-    @param tabuleiro Tabuleiro possivelmente já com peças fixadas
-    @param peca_atual Peça atual ainda não fixada pelo jogador 
-    @param pontuacao Pontuação do jogador
     """
+    Classe:     Partida
+    Resumo: Implementa todos os aspectos inerentes a uma instância de partida de    Textris
+    Detalhes: Atribui a um objeto as características de linhas, colunas, tabuleiro - que se trata da combinação de linhas e colunas, além
+    das peças já fixadas, peças que ainda não foram fixadas - mas já apareceram no tabuleiro (parâmetro importante especialmente para a 
+    funcionalidade de gravar o jogo), do jogador e sua respectiva pontuação.
+    """
+
     def __init__(self, linhas, colunas, jogador, tabuleiro=None, peca_atual=None, pontuacao=None):
+        """
+        Resumo: Construtor para uma instância de Partida
+
+        Detalhes: As linhas e colunas formam o tabuleiro no qual os blocos se empilharão, cada partida também tem necessariamente um jogador 
+        atribuído a ela. Os argumentos opcionais "tabuleiro", "peca_atual" e "pontuacao" são passados para carregar uma partida 
+        previamente gravada.
+
+        Parâmetro linhas: Quantidade de linhas do tabuleiro 
+        Parâmetro colunas: Quantidade de colunas do tabuleiro 
+        Parâmetro jogador: Jogador da partida
+        Parâmetro tabuleiro: Tabuleiro possivelmente já com peças fixadas
+        Parâmetro peca_atual: Peça atual ainda não fixada pelo jogador 
+        Parâmetro pontuacao: Pontuação do jogador
+        """
         self.linhas = linhas
         self.colunas = colunas
         self.jogador = jogador
@@ -99,41 +96,40 @@ class Partida:
             self.peca_atual = self._gerar_peca()
             self.pontuacao = 0
         
-    """!
-    @brief Gera uma peça
-
-    @details Gera um tetrimino de formato selecionado pseudo-aleatoriamente entre todas as possibilidades e com posição inicial 
-    no topo das colunas e região central das linhas
-    """
     def _gerar_peca(self):
-        #Gera a peça aleatória
+        """
+        Resumo: Gera uma peça
+    
+        Detalhes: Gera um tetrimino de formato selecionado pseudo-aleatoriamente entre todas as possibilidades e com posição inicial 
+        no topo das colunas e região central das linhas
+        """
         tipos = ["I", "O", "T", "S", "Z", "J", "L"]
         return Peca(random.choice(tipos), self.colunas//2 - 2)
     
-    """!
-    @brief Move a peça
-    
-    @details Atualiza a posição da peça de acordo com as coordenadas x e y novas
-
-    @param dx Distância entre a coordenada vertical da peça e a linha mais acima no tabuleiro
-    @param dy Distância entre a coordenada horizontal da peça e a coluna mais à esquerda no tabuleiro
-    """
     def mover_peca(self, dx, dy):
+        """
+        Resumo: Move a peça
+        
+        Detalhes: Atualiza a posição da peça de acordo com as coordenadas x e y novas
+    
+        Parâmetro dx: Distância entre a coordenada vertical da peça e a linha mais acima no tabuleiro
+        Parâmetro dy: Distância entre a coordenada horizontal da peça e a coluna mais à esquerda no tabuleiro
+        """
         if self._pode_mover(dx, dy):
             self.peca_atual.x += dx
             self.peca_atual.y += dy
         elif dx == 1: 
             self._fixar_peca()
 
-    """!
-    @brief Verifica se é possível mover a peça
-    
-    @param dx Distância entre a coordenada vertical da peça e a linha mais acima no tabuleiro
-    @param dy Distância entre a coordenada horizontal da peça e a coluna mais à esquerda no tabuleiro 
-
-    @return Verdadeiro se o deslocamento é possível, falso caso contrário
-    """
     def _pode_mover(self, dx, dy):
+        """
+        Resumo: Verifica se é possível mover a peça
+        
+        Parâmetro dx: Distância entre a coordenada vertical da peça e a linha mais acima no tabuleiro
+        Parâmetro dy: Distância entre a coordenada horizontal da peça e a coluna mais à esquerda no tabuleiro 
+    
+        Retorna: Verdadeiro se o deslocamento é possível, falso caso contrário
+        """
         for i, row in enumerate(self.peca_atual.matriz):
             for j, bloco in enumerate(row):
                 if bloco != ".":
@@ -143,12 +139,12 @@ class Partida:
                         return False
         return True
 
-    """!
-    @brief Rotaciona a peça
-    
-    @param posicao Posição atual da peça
-    """
     def rotacionar_peca(self, posicao):
+        """
+        Resumo: Rotaciona a peça
+        
+        Parâmetro posicao: Posição atual da peça
+        """
         antiga_matriz = self.peca_atual.matriz
         antiga_rotacao = self.peca_atual.rotacao
         self.peca_atual.rotacionar(posicao)
@@ -157,10 +153,10 @@ class Partida:
             self.peca_atual.matriz = antiga_matriz
             self.peca_atual.rotacao = antiga_rotacao
 
-    """!
-    @brief Fixa a peça no tabuleiro
-    """
     def _fixar_peca(self):
+        """
+        Resumo: Fixa a peça no tabuleiro
+        """
         #Coloca a peça no tabuleiro principal
         for i, row in enumerate(self.peca_atual.matriz):
             for j, bloco in enumerate(row):
@@ -173,19 +169,19 @@ class Partida:
         if not self._pode_mover(0, 0):  
             self.game_over = True
 
-    """!
-    @brief Remove as linhas completas
-    """
     def _remover_linhas_completas(self):
+        """
+        Resumo: Remove as linhas completas
+        """
         linhas_incompletas = [row for row in self.tabuleiro if "." in row]
         removidas = self.linhas - len(linhas_incompletas)
         self.pontuacao += removidas
         self.tabuleiro = [["." for _ in range(self.colunas)] for _ in range(removidas)] + linhas_incompletas
 
-    """!
-    @brief Imprime o tabuleiro no estado atual da partida
-    """
     def imprimir_tabuleiro(self):
+        """
+        Resumo: Imprime o tabuleiro no estado atual da partida
+        """
         #Imprime o estado do tabuleiro com a peça que o usuário está movimentando
         tabuleiro_temporario = [row[:] for row in self.tabuleiro]
 
@@ -199,51 +195,51 @@ class Partida:
         for row in tabuleiro_temporario:
             print("".join(row))
 
-    """!
-    @brief Salva a partida com o nome do jogador, data e horário atuais
-    """
     def salvar_partida(self):
+        """
+        Resumo: Salva a partida com o nome do jogador, data e horário atuais
+        """
         horario = datetime.datetime.now().strftime("%d-%m-%Y-%H:%M")
         nomeArq = self.jogador + "-" + horario + ".pkl" # .pkl se trata da extensão de arquivo utilizada pelo módulo pickle
         with open(nomeArq, 'wb') as a:
             pickle.dump(Partida(self.linhas, self.colunas, self.jogador, self.tabuleiro, self.peca_atual, self.pontuacao), a)
         
-"""!
-@class Jogo
-
-@brief Implementa as características do jogo de Textris.
-
-@details Classe que atribui dinamização à classe Partida, isto é, realiza o intermédio entre a classe Partida e input do usuário,
-conferindo, apropriadamente, o aspecto de um jogo à instância de partida.   
-"""
 class Jogo:
-    """!
-    @brief Construtor para uma instância de Jogo
-
-    @details Todos os argumentos serão passados para a criação de uma instância de Partida, a qual está um nível de hierarquia abaixo
-    da classe Jogo.
-
-    @param linhas Quantidade de linhas do tabuleiro 
-    @param colunas Quantidade de colunas do tabuleiro 
-    @param jogador Jogador da partida
-    @param tabuleiro Tabuleiro possivelmente já com peças fixadas
-    @param peca_atual Peça atual ainda não fixada pelo jogador 
-    @param pontuacao Pontuação do jogador
+    """
+    Classe: Jogo
+    
+    Resumo: Implementa as características do jogo de Textris.
+    
+    Detalhes: Classe que atribui dinamização à classe Partida, isto é, realiza o intermédio entre a classe Partida e input do usuário,
+    conferindo, apropriadamente, o aspecto de um jogo à instância de partida.   
     """
     def __init__(self, linhas, colunas, jogador, tabuleiro=None, peca_atual=None, pontuacao=None):
+        """
+        Resumo: Construtor para uma instância de Jogo
+    
+        Detalhes: Todos os argumentos serão passados para a criação de uma instância de Partida, a qual está um nível de hierarquia abaixo
+        da classe Jogo.
+    
+        Parâmetro linhas: Quantidade de linhas do tabuleiro 
+        Parâmetro colunas: Quantidade de colunas do tabuleiro 
+        Parâmetro jogador: Jogador da partida
+        Parâmetro tabuleiro: Tabuleiro possivelmente já com peças fixadas
+        Parâmetro peca_atual: Peça atual ainda não fixada pelo jogador 
+        Parâmetro pontuacao: Pontuação do jogador
+        """
         self.salvar = False
         if tabuleiro is not None:
             self.partida = Partida(linhas, colunas, jogador, tabuleiro, peca_atual, pontuacao)
         else:
             self.partida = Partida(linhas, colunas, jogador)
 
-    """!
-    @brief Inicia uma partida de Textris
-
-    @details Através de funções para a leitura de input, a função constantemente realiza as atualizações necessárias
-    no tabuleiro simultaneamente ao input do jogador
-    """
     def iniciar(self):
+        """
+        Resumo: Inicia uma partida de Textris
+    
+        Detalhes: Através de funções para a leitura de input, a função constantemente realiza as atualizações necessárias
+        no tabuleiro simultaneamente ao input do jogador
+        """
         self.salvar = False
         os.system('cls||clear')
         while not(self.partida.game_over):
@@ -276,34 +272,34 @@ class Jogo:
             print("Fim de partida!")
             print("Pontuação final: ", self.partida.pontuacao, "\n")
 
-    """!
-    @brief Retorna o estado de gravação do jogo
-    """
     def jogo_salvo(self):
+        """
+        Resumo: Retorna o estado de gravação do jogo
+        """
         return self.salvar
 
-"""!
-@class Ranking
-
-@brief Administra a colocação dos jogadores
-"""
 class Ranking:
-    """!
-    @brief Construtor para o sistema de colocação do Textris
+    """
+    Classe: Ranking
+    
+    Resumo: Administra a colocação dos jogadores
     """
     def __init__(self):
+        """
+        Resumo: Construtor para o sistema de colocação do Textris
+        """
         self.rank = [[i for i in range(-1, 1)] for _ in range(10)]
     
-    """!
-    @brief Atualiza a colocação de acordo com uma nova pontuação
-    
-    @details Itera sobre as 10 posições e, caso faça sentido, insere uma nova colocação de acordo com a nova pontuação, realizando os ajustes
-    necessário para as colocações anteriores
-
-    @param jogador Nome do jogador
-    @param pontuacao Pontuação atribuída ao jogador
-    """
     def atualizar_ranking(self, jogador, pontuacao):
+        """
+        Resumo: Atualiza a colocação de acordo com uma nova pontuação
+        
+        Detalhes: Itera sobre as 10 posições e, caso faça sentido, insere uma nova colocação de acordo com a nova pontuação, realizando os ajustes
+        necessário para as colocações anteriores
+    
+        Parâmetro jogador: Nome do jogador
+        Parâmetro pontuacao: Pontuação atribuída ao jogador
+        """
         for i in range (10):
             if pontuacao > self.rank[i][0]:
                 for j in range (8, i - 1, -1):
@@ -311,22 +307,22 @@ class Ranking:
                 self.rank[i] = [pontuacao, jogador]
                 break
     
-    """!
-    @brief Imprime as colocações dos jogadores
-    """
     def mostrar_ranking(self):
+        """
+        Resumo: Imprime as colocações dos jogadores
+        """
         for i in range(10):
             if(-1 == self.rank[i][0]):
                 print(i + 1, ". " , sep="")
             else:
                 print(i + 1, ". ", self.rank[i][0], " ", self.rank[i][1], sep="")
 
-"""!
-@brief Inicia um jogo novo ou carrega um jogo já existente
-
-@param arq_partida_salva Nome de um arquivo que contém informações de uma partida previamente gravada por um jogador
-"""
 def iniciar_partida(arq_partida_salva=None):
+    """
+    Resumo: Inicia um jogo novo ou carrega um jogo já existente
+
+    Parâmetro arq_partida_salva: Nome de um arquivo que contém informações de uma partida previamente gravada por um jogador
+    """
     if arq_partida_salva is not None:
         #inicia um jogo já salvo
         with open(arq_partida_salva, 'rb') as a:
@@ -350,10 +346,10 @@ def iniciar_partida(arq_partida_salva=None):
     if(not(jogo.jogo_salvo())):
         rank.atualizar_ranking(jogador, jogo.partida.pontuacao)
 
-"""!
-@brief Carrega uma partida salva anteriormente
-"""
 def carregar_partida():
+    """
+    Resumo: Carrega uma partida salva anteriormente
+    """
     os.system('cls||clear')
     caminho_pasta = '.' #indica a pasta atual
     arqs_pkl = [arq for arq in os.listdir(caminho_pasta) if arq.endswith('.pkl')]
@@ -377,10 +373,10 @@ def carregar_partida():
             print("Escolha inválida, tente novamente:\n")
             print(" ")
 
-"""!
-@brief Mostra o ranking de colocações dos jogadores
-"""
 def ver_melhores_pontuacoes():
+    """
+    Resumo: Mostra o ranking de colocações dos jogadores
+    """
     os.system('cls||clear')
     print("*** Jogo Textris - Melhores pontuações ***")
     rank.mostrar_ranking()
